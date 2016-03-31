d264fe2b515abe2b04a631dd781ab855674903a4
Viri <Viri@192.168.1.71>
2016-03-30 22:34:43 -0600

    Original

---
+++ Practica05/src/main/java/edu/uacm/Application.java
@@ -0,0 +1,16 @@
+package edu.uacm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //orientado a notaciones, el framework por atras va a hacer muchas cosas
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		
+	}
+}
+
+//la parte de servicios es orientado a interfaces
+
